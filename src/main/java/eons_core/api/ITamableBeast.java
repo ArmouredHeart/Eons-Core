76a06ac8c29198b2b87e91936b3c2b9610e38572
ArmouredHeart <42709948+ArmouredHeart@users.noreply.github.com>
2020-02-11 11:51:33 -0400

    Initial upload :)

---
+++ src/main/java/eons_core/api/ITamableBeast.java
@@ -0,0 +1,14 @@
+// package
+package eons_core;
+
+// Forge imports
+
+// Eons imports
+
+// misc imports
+
+/** extend Tamable interface from vanilla minecraft*/
+/*public interface ITamableBeast extends ITamable
+{
+
+}*/
